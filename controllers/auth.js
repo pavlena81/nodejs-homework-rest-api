dc18d1b47e58a41122b60b6c65eeb0bd185abc8a
Olena Nesteruk <pavlena81@gmail.com>
2023-01-08 22:14:17 +0200

    adds login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,12 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require("dotenv").config()
 
 const { User } = require('../models/user')
 
 const { HttpError, ctrlWrapper } = require('../helpers')
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
 
@@ -22,6 +26,33 @@ const register = async (req, res) => {
     })
 }
 
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw HttpError(401, 'Email or password invalid');
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+         throw HttpError(401, 'Email or password invalid');
+    }
+
+    const payload = {
+        id: user._id,
+    }
+
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
+
+    res.json({
+        token,
+        name: user.name,
+        email: user.email,
+    })
+}
+
 module.exports = {
     register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
 }
